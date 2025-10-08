87503f4c360e3b08318038e34fddb124e3875ca0
adrianmontiel0 <156972771+adrianmontiel0@users.noreply.github.com>
2025-10-08 14:15:21 -0600

    Update data/__init__.py
    Basic code for loading data to the project.

--- src/mlops/data/__init__.py
+++ src/mlops/data/__init__.py
@@ -1,1 +1,9 @@
+import pandas as pd
+from pathlib import Path
 
+def load_data(file_path: str) -> pd.DataFrame:
+  """Carga de CSV y devolución de DataFrame."""
+  path = Path(file_path)
+  if not path.exists():
+    raise FileNotFoundError(f"No se encontro el archivo: {file_path}")
+  return pd.read_csv(path)
